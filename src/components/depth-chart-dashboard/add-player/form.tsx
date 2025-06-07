ba6dbf0a8ef3512e552138f305310e58acc285c5
Michelle Teoh <work@michelleteoh.me>
2025-06-07 16:08:27 +0800

    feat: Add player form with position, sport, spot, and name submission

---
+++ src/components/depth-chart-dashboard/add-player/form.tsx
@@ -0,0 +1,167 @@
+import {
+  Button,
+  createListCollection,
+  Field,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
+import { type RefObject } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+import DropdownSelect from '../../ui/select';
+
+import { useAppDispatch, useAppSelector } from '../../../store/hook';
+import { addPlayer } from '../../../store/slices/sports';
+
+interface FormValues {
+  playerName: string;
+  position: string;
+  sport: string;
+  spot: string;
+}
+
+type Option = { label: string; value: string };
+
+export function AddPlayerForm({
+  contentRef,
+}: {
+  contentRef: RefObject<HTMLElement | null>;
+}) {
+  const sports = useAppSelector((state) => state.sports.sports);
+  const dispatch = useAppDispatch();
+
+  const form = useForm<FormValues>();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = form;
+
+  const selectedSport = form.watch('sport');
+
+  const sportOptions: { items: Option[] } = createListCollection({
+    items: sports.map((sport) => ({
+      label: sport.name,
+      value: sport.name,
+    })),
+  });
+
+  const selectedSportData = sports.find(
+    (sport) => sport.name === selectedSport
+  );
+
+  const positionOptions: { items: Option[] } = createListCollection({
+    items:
+      selectedSportData?.position?.map((pos) => ({
+        label: pos.name,
+        value: pos.name,
+      })) || [],
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+
+    dispatch(
+      addPlayer({
+        sportName: data.sport,
+        positionName: data.position,
+        spot: data.spot,
+        playerName: data.playerName,
+      })
+    );
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Stack gap="4" align="flex-start" maxW="sm">
+        <Field.Root invalid={!!errors.sport}>
+          <Field.Label>Sports</Field.Label>
+          <Controller
+            control={control}
+            name="sport"
+            render={({ field }) => (
+              <DropdownSelect
+                defaultValue={[sportOptions.items[0].value]}
+                name={field.name}
+                options={sportOptions}
+                parentRef={contentRef}
+                placeholder="Select sport type"
+                value={[field.value]}
+                onInteractOutside={() => field.onBlur()}
+                onValueChange={(option: { value: string[] }) =>
+                  field.onChange(option.value[0])
+                }
+              />
+            )}
+          />
+          <Field.ErrorText>{errors.sport?.message}</Field.ErrorText>
+        </Field.Root>
+
+        {positionOptions.items.length > 0 && (
+          <Field.Root invalid={!!errors.position}>
+            <Field.Label>Position</Field.Label>
+            <Controller
+              control={control}
+              name="position"
+              render={({ field }) => (
+                <DropdownSelect
+                  name={field.name}
+                  options={positionOptions}
+                  parentRef={contentRef}
+                  placeholder="Select a position"
+                  value={[field.value]}
+                  onInteractOutside={() => field.onBlur()}
+                  onValueChange={(option: { value: string[] }) =>
+                    field.onChange(option.value[0])
+                  }
+                />
+              )}
+            />
+            <Field.ErrorText>{errors.position?.message}</Field.ErrorText>
+          </Field.Root>
+        )}
+
+        <Field.Root invalid={!!errors.playerName}>
+          <Field.Label>Player Name</Field.Label>
+          <Input {...register('playerName')} />
+          <Field.ErrorText>{errors.playerName?.message}</Field.ErrorText>
+        </Field.Root>
+
+        <Field.Root invalid={!!errors.spot}>
+          <Field.Label>Spot</Field.Label>
+          <Controller
+            control={control}
+            name="spot"
+            render={({ field }) => (
+              <DropdownSelect
+                name={field.name}
+                value={[field.value]}
+                options={spotOptions}
+                placeholder="Select a spot"
+                parentRef={contentRef}
+                onInteractOutside={() => field.onBlur()}
+                onValueChange={(option: { value: number[] }) =>
+                  field.onChange(option.value[0])
+                }
+              />
+            )}
+          />
+          <Field.ErrorText>{errors.position?.message}</Field.ErrorText>
+        </Field.Root>
+
+        <Button type="submit">Add Player</Button>
+      </Stack>
+    </form>
+  );
+}
+
+const spotOptions = createListCollection({
+  items: [
+    { label: 'Starter', value: '0' },
+    { label: 'Second', value: '1' },
+    { label: 'Third', value: '2' },
+    { label: 'Forth', value: '3' },
+  ],
+});
