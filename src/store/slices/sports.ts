df3824503b4fddccbde8a28a999cf27bad30df04
Michelle Teoh <work@michelleteoh.me>
2025-06-07 16:02:01 +0800

    feat: Add addPlayer reducer to store player info

---
+++ src/store/slices/sports.ts
@@ -0,0 +1,67 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import mockSportsData from '../../mock/sports.json';
+
+import { MAX_PLAYERS } from '../../utility/constants';
+
+type Player = string;
+
+type Position = {
+  name: string;
+  spots: Player[];
+};
+
+type Sport = {
+  name: string;
+  position: Position[];
+};
+
+type SportsState = {
+  sports: Sport[];
+};
+
+const initialState: SportsState = mockSportsData;
+
+const sportsSlice = createSlice({
+  name: 'sports',
+  initialState,
+  reducers: {
+    addPlayer: (
+      state,
+      action: PayloadAction<{
+        sportName: string;
+        positionName: string;
+        spot?: string;
+        playerName: string;
+      }>
+    ) => {
+      const {
+        sportName,
+        positionName,
+        spot: spotIndex,
+        playerName,
+      } = action.payload;
+
+      const sport = state.sports.find((sport) => sport.name === sportName);
+
+      if (!sport) return;
+
+      const position = sport.position.find((pos) => pos.name === positionName);
+
+      if (!position) return;
+
+      position.spots.splice(
+        spotIndex ? Number(spotIndex) : position.spots.length - 1,
+        0,
+        playerName
+      );
+
+      if (position.spots.length > 4) {
+        position.spots = position.spots.slice(0, MAX_PLAYERS);
+      }
+    },
+  },
+});
+
+export const { addPlayer } = sportsSlice.actions;
+export default sportsSlice.reducer;
