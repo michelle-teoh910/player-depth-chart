87b937c424b72e8abe6a77d9433720ede311e0f8
Michelle Teoh <work@michelleteoh.me>
2025-06-08 13:50:16 +0800

    refactor: Extract ts def for DropdownSelect comp

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -4,12 +4,7 @@ import { type RefObject } from 'react';
 
 type Option = { label: string; value: string };
 
-export default function DropdownSelect({
-  options,
-  parentRef,
-  placeholder = '',
-  ...rest
-}: Omit<SelectRootProps, 'collection'> & {
+type DropdownSelectProps = Omit<SelectRootProps, 'collection'> & {
   options: { items: Option[] };
   /**
    * Use this only when the dropdown is rendered inside a dialog.
@@ -18,10 +13,18 @@ export default function DropdownSelect({
   parentRef?: RefObject<HTMLElement | null>;
   placeholder?: string;
   value: string[];
-}) {
+};
+
+export default function DropdownSelect({
+  options,
+  parentRef,
+  placeholder = '',
+  ...rest
+}: DropdownSelectProps) {
   return (
     <Select.Root collection={options} {...rest}>
       <Select.HiddenSelect />
+
       <Select.Control>
         <Select.Trigger>
           <Select.ValueText placeholder={placeholder} />
@@ -30,6 +33,7 @@ export default function DropdownSelect({
           <Select.Indicator />
         </Select.IndicatorGroup>
       </Select.Control>
+
       <Portal container={parentRef}>
         <Select.Positioner>
           <Select.Content>
